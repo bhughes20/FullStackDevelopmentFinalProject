8d8085dddff4f3b5ecda080c9a403983de1d75c8
bhughes20 <bhughes567@gmail.com>
2021-11-23 15:49:15 +0000

    Toast container autoClose time updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
     <ChakraProvider theme={theme}>
       <ToastContainer
         position="top-center"
-        autoClose={3000}
+        autoClose={2000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
