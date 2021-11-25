fd8bd58090e758c149aa83903853ebdaf5124135
bhughes20 <bhughes567@gmail.com>
2021-11-25 15:46:41 +0000

    Routing to display Not Found Message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,34 +11,34 @@ import Admin from "./components/Admin/Admin";
 import DriverDetails from "./components/DriverDetails/DriverDetails";
 
 function App() {
-  return (
-    <ChakraProvider theme={theme}>
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      <HashRouter>
-        <Navbar />
-        <Switch>
-            <Route exact path="/" component={QuoteForm} />
-            <Route exact path="/admin" component={Admin} />
-            <Route path="/driver-details/:id" component={DriverDetails} />
-            <Route component={NotFound} />
-        </Switch>
-      </HashRouter>
-    </ChakraProvider>
-  );
+    return (
+        <ChakraProvider theme={theme}>
+            <ToastContainer
+                position="top-center"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+            <HashRouter>
+                <Navbar/>
+                <Switch>
+                    <Route exact path="/" component={QuoteForm}/>
+                    <Route exact path="/admin" component={Admin}/>
+                    <Route exact path="/driver-details/:id" component={DriverDetails}/>
+                    <Route component={NotFound}/>
+                </Switch>
+            </HashRouter>
+        </ChakraProvider>
+    );
 }
+
+export default App;
 
 function NotFound() {
     return <>You have landed on a page that doesn't exist</>;
 }
-
-export default App;
